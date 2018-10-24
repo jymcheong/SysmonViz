e663e621ebba5bb0106c9fe224d10800939267e7
jymcheong <jym@jym.sg>
2018-10-24 14:28:55 +0800

    added orientJS 3.x scripts

---
+++ serverSide/handleHostProcessGuid.js
@@ -0,0 +1,74 @@
+var _edgeLookup = {'ProcessTerminate':'Terminated', 'PipeCreated':'CreatedPipe',
+                    'PipeConnected':'ConnectedPipe', 'RawAccessRead':'RawRead',
+                    'FileCreateTime':'ChangedFileCreateTime', 'FileCreate':'CreatedFile',
+                    'FileCreateStreamHash':'CreatedFileStream', 'RegistryEvent':'AccessedRegistry',
+                    'NetworkConnect':'ConnectedTo', 'ImageLoad':'LoadedImage'}
+
+const fs = require("fs")
+const _cachedir = __dirname + '/RIDcache';
+eval(fs.readFileSync(__dirname + '/cleanup.js')+'');
+
+// this will hold the HostnameProcessGuid -> @rid
+if (!fs.existsSync(_cachedir)){
+    fs.mkdirSync(_cachedir);
+}
+
+const OrientDBClient = require("orientjs").OrientDBClient
+OrientDBClient.connect({ host: "172.30.1.178",port: 2424})
+.then(client => {
+    _client = client; //used in cleanup.js
+    client.session({ name: "DataFusion", username: "root", password: "Password1234" })
+    .then(session => {
+        console.log('session opened')
+        _session = session //used in cleanup.js
+        _handle = session.liveQuery("select from V") //used in cleanup.js
+        .on("data", data => {
+            if(data['operation'] != 1) return               
+            //console.log('Processing ' + data['data']['Image'] + ' from ' + data['data']['Hostname']);
+            findProcessCreate(data['data'])
+        })
+    })
+})
+
+function linkNewEvent(classname, sourceRID, targetRID){
+        //console.log(classname + ' link ' + sourceRID + ' to ' + targetRID)
+        sql = 'CREATE EDGE ' + _edgeLookup[classname] + ' FROM ' + sourceRID + ' TO ' + targetRID
+        console.log(sql)
+        _session.command(sql)
+        // if ProcessTerminate, clean up file cache....
+}
+
+function findProcessCreate(newEvent) {
+    if(newEvent['Image'] == 'System') return
+    switch(newEvent['@class']){
+        case "ProcessTerminate"://ID5: ProcessCreate-[Terminated]->ProcessTerminate     	
+        case "PipeCreated":	    //ID17: ProcessCreate-[CreatedPipe]->PipeCreated	
+        case "PipeConnected":   //ID18: ProcessCreate-[ConnectedPipe]->PipeConnected
+        case "RawAccessRead":   //ID9: ProcessCreate-[RawRead]->RawAccessRead
+        case "FileCreateTime":  //ID2: ProcessCreate-[ChangedFileCreateTime]->FileCreateTime	
+        case "FileCreate": 	    //ID11: ProcessCreate-[CreatedFile]->FileCreate 
+        case "FileCreateStreamHash": //ID15: ProcessCreate-[CreatedFileStream]->FileCreateStreamHash    
+        case "RegistryEvent":   //ID13&14: ProcessCreate-[AccessedRegistry]->RegistryEvent
+        case "NetworkConnect":  //ID3: ProcessCreate-[ConnectedTo]->NetworkConnect 
+            // find processcreate @rid from cache
+            fs.readFile(_cachedir + '/' + newEvent['Hostname'] + newEvent['ProcessGuid'], function(err, processCreateRid){
+                if(err) { // not in cache, find in database
+                    console.log('Trying database to find for ' + newEvent['@rid']+ '...')
+                    _session.query("select @rid from (select from processcreate where ProcessGuid = :guid) where Hostname = :hostname", 
+                    { params : {guid: newEvent['ProcessGuid'], hostname: newEvent['Hostname']}})
+                    .all()
+                    .then((results)=> {
+                        if(results.length == 0) {
+                            console.log('cannot find processcreate for ' + newEvent['@rid'])
+                            return
+                        }
+                        fs.writeFile(_cachedir + '/' + newEvent['Hostname'] + newEvent['ProcessGuid'], results[0]['@rid'], function(err) { if(err) console.log(err) });
+                        linkNewEvent(newEvent['@class'],results[0]['@rid'], newEvent['@rid'])
+                    });
+                    return
+                }
+                linkNewEvent(newEvent['@class'],processCreateRid, newEvent['@rid'])
+            })   
+            break;
+    }
+}
---
+++ serverSide/handleProcessCreate.js
@@ -0,0 +1,72 @@
+const fs = require("fs")
+const _cachedir = __dirname + '/RIDcache';
+eval(fs.readFileSync(__dirname + '/cleanup.js')+'');
+
+// this will hold the HostnameProcessGuid -> @rid
+if (!fs.existsSync(_cachedir)){
+    fs.mkdirSync(_cachedir);
+}
+
+const OrientDBClient = require("orientjs").OrientDBClient
+OrientDBClient.connect({ host: "172.30.1.178",port: 2424})
+.then(client => {
+    _client = client; //used in cleanup.js
+    client.session({ name: "DataFusion", username: "root", password: "Password1234" })
+    .then(session => {
+        console.log('session opened')
+        _session = session //used in cleanup.js
+        _handle = session.liveQuery("select from processcreate") //used in cleanup.js
+        .on("data", data => {
+            if(data['operation'] != 1) return               
+            console.log('Processing ' + data['data']['Image'] + ' from ' + data['data']['Hostname']);
+            findParent(data['data'])
+        })
+    })
+})
+
+function connectParentOf(sourceRID, targetRID) {
+    console.log('creating ParentOf from ' + sourceRID + ' to ' + targetRID)
+    _session.command('CREATE EDGE ParentOf FROM ' + sourceRID + ' TO ' + targetRID)
+    .on('data',(results)=> {
+        //console.log("Updating ToBeProcessed for " + targetRID)
+        _session.command('Update ' + targetRID + 'SET ToBeProcessed = false')
+        .on('error',(err)=> {
+            console.log(err)
+            console.log('Retrying update...')
+            _session.command('Update ' + targetRID + 'SET ToBeProcessed = false')
+        })
+        .on('data',(data)=> {
+            console.log('updated processcreate ' + targetRID)
+            // do 'gaps once here
+        })
+        // get parentOf sequence
+    })
+    .on('error',(err)=> {
+        console.log('Retrying creating ParentOf from ' + sourceRID + ' to ' + targetRID)
+        connectParentOf(sourceRID, targetRID)
+    })
+}
+
+function findParent(newpc) {
+    // write new @rid to cache folder
+    fs.writeFile(_cachedir + '/' + newpc['Hostname'] + newpc['ProcessGuid'], newpc['@rid'], function(err) { if(err) console.log(err) });
+    // find parent @rid in cache
+    fs.readFile(_cachedir + '/' + newpc['Hostname'] + newpc['ParentProcessGuid'], function(err, parentrid){
+        if(err) { // not in cache, find in database
+            console.log('Cannot find parent rid in cache, trying database...')
+            _session.query("select @rid from (select from processcreate where ProcessGuid = :guid) where Hostname = :hostname", 
+            { params : {guid: newpc['ParentProcessGuid'], hostname: newpc['Hostname']}})
+            .all()
+            .then((results)=> {
+                if(results[0]['@rid']===undefined) {
+                    console.log('cannot find parent for ' + newpc['@rid'])
+                    return
+                }
+                fs.writeFile(_cachedir + '/' + newpc['Hostname'] + newpc['ParentProcessGuid'], results[0]['@rid'], function(err) { if(err) console.log(err) });
+                connectParentOf(results[0]['@rid'], newpc['@rid'])
+            });
+            return
+        }
+        connectParentOf(parentrid, newpc['@rid'])
+    })   
+}
