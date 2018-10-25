2c4fd669bac486c7bf03b8265c902b76dfda5005
jc <jym@jym.sg>
2018-10-25 10:24:30 +0800

    linking tracking classes to ProcessCreate

--- sampleInjection/i.js
+++ sampleInjection/i.js
@@ -4,19 +4,6 @@ var OrientDB = require('orientjs');
 var server = OrientDB({host: 'localhost', port: 2424});
 var db = server.use({name: 'DataFusion', username: ODB_User, password: ODB_pass, useToken : false});
 
-process.stdin.resume();//so the program will not close instantly
-function exitHandler(err) {
-    console.log('cleaning up...')
-    db.close().then(function(){
-        process.exit();
-    })
-}
-process.on('exit', exitHandler.bind(null));
-process.on('SIGINT', exitHandler.bind(null));
-process.on('SIGUSR1', exitHandler.bind(null));
-process.on('SIGUSR2', exitHandler.bind(null));
-process.on('uncaughtException', exitHandler.bind(null));
-
 function processFile(inputFile) {
     var fs = require('fs'),
         readline = require('readline'),
