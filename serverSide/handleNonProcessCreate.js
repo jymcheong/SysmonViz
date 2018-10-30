995e9f305e8e3d5eb49c5fbc2d0f54007029f1d6
jymcheong <jym@jym.sg>
2018-10-30 09:18:53 +0800

    LQ on Sysmon instead of V class

--- serverSide/handleNonProcessCreate.js
+++ serverSide/handleNonProcessCreate.js
@@ -1,25 +1,33 @@
 const fs = require("fs")
 eval(fs.readFileSync(__dirname + '/common.js')+'');
 
-startLiveQuery("select from V")
+startLiveQuery("select from Sysmon")
 
 function linkNewEvent(classname, sourceRID, targetRID){
         //console.log(classname + ' link ' + sourceRID + ' to ' + targetRID)
         sql = 'CREATE EDGE ' + _edgeLookup[classname] + ' FROM ' + sourceRID + ' TO ' + targetRID
         _session.command(sql)
         .on("data", data => {
-            console.log('Connected ' + _edgeLookup[classname] + ' FROM ' + sourceRID + ' TO ' + targetRID)
+            console.log('Connecting ' + _edgeLookup[classname] + ' FROM ' + sourceRID + ' TO ' + targetRID)
             _session.command('UPDATE '+ targetRID + ' SET ToBeProcessed = false')
         })
         .on('error',(err)=> {
             console.log(err)
         })
         // if ProcessTerminate, clean up file cache....
+        if(classname == 'ProcessTerminate') console.log('Cleaning up cache...')
+        /*
+            _cacheProcessCreateRID should be last because need @rid to link other events first
+        */
 }
 
 function eventHandler(newEvent) {
     if(newEvent['Image'] == 'System') return
     switch(newEvent['@class']){
+        case "ImageLoad":
+            console.log('Ignoring ImageLoad')
+            break;
+
         case "ProcessTerminate"://ID5: ProcessCreate-[Terminated]->ProcessTerminate     	
         case "PipeCreated":	    //ID17: ProcessCreate-[CreatedPipe]->PipeCreated	
         case "PipeConnected":   //ID18: ProcessCreate-[ConnectedPipe]->PipeConnected
@@ -45,10 +53,9 @@ function eventHandler(newEvent) {
                             linkNewEvent(newEvent['@class'],results[0]['@rid'], newEvent['@rid'])
                         }
                     });
+                    return
                 }
-                else{
-                    linkNewEvent(newEvent['@class'],processCreateRid, newEvent['@rid'])
-                }
+                linkNewEvent(newEvent['@class'],processCreateRid, newEvent['@rid'])
             })   
             break;
     }
