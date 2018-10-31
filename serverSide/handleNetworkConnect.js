e75ab38eb7fe3e9ed9063f7b3f55d38164481574
jymcheong <jym@jym.sg>
2018-10-31 17:14:30 +0800

    fix missing link

--- serverSide/handleNetworkConnect.js
+++ serverSide/handleNetworkConnect.js
@@ -6,35 +6,20 @@ startLiveQuery("select from Sysmon")
 function linkNewEvent(classname, sourceRID, targetRID){
         //console.log(classname + ' link ' + sourceRID + ' to ' + targetRID)
         sql = 'CREATE EDGE ' + _edgeLookup[classname] + ' FROM ' + sourceRID + ' TO ' + targetRID
-        if(classname == 'FileCreate') console.log(sql)
         _session.command(sql)
         .on("data", data => {
-            if(classname == 'FileCreate') console.log('Connecting ' + _edgeLookup[classname] + ' FROM ' + sourceRID + ' TO ' + targetRID)
+            console.log('Connecting ' + _edgeLookup[classname] + ' FROM ' + sourceRID + ' TO ' + targetRID)
             _session.command('UPDATE '+ targetRID + ' SET ToBeProcessed = false')
         })
         .on('error',(err)=> {
             console.log(err)
         })
-        // if ProcessTerminate, clean up file cache....
-        //if(classname == 'ProcessTerminate') console.log('Cleaning up cache...')
-        /*
-            _cacheProcessCreateRID should be last because need @rid to link other events first
-        */
 }
 
 function eventHandler(newEvent) {
     if(newEvent['Image'] == 'System') return
     switch(newEvent['@class']){
-        case "ImageLoad":
-        case "ProcessTerminate"://ID5: ProcessCreate-[Terminated]->ProcessTerminate     	
-        case "PipeCreated":	    //ID17: ProcessCreate-[CreatedPipe]->PipeCreated	
-        case "PipeConnected":   //ID18: ProcessCreate-[ConnectedPipe]->PipeConnected
-        case "RawAccessRead":   //ID9: ProcessCreate-[RawRead]->RawAccessRead
-        case "FileCreateTime":  //ID2: ProcessCreate-[ChangedFileCreateTime]->FileCreateTime	
-        case "FileCreate": 	    //ID11: ProcessCreate-[CreatedFile]->FileCreate 
-        case "FileCreateStreamHash": //ID15: ProcessCreate-[CreatedFileStream]->FileCreateStreamHash    
-        case "RegistryEvent":   //ID13&14: ProcessCreate-[AccessedRegistry]->RegistryEvent
-        //case "NetworkConnect":  //ID3: ProcessCreate-[ConnectedTo]->NetworkConnect 
+      case "NetworkConnect":  //ID3: ProcessCreate-[ConnectedTo]->NetworkConnect 
             if(newEvent['@class'] == 'FileCreate') console.log('FileCreate came in...' + newEvent['@rid'])
             fs.readFile(_cacheProcessCreateRID + '/' + newEvent['Hostname'] + newEvent['ProcessGuid'], function(err, processCreateRid){
                 if(err) { // not in cache, find in database
@@ -43,7 +28,7 @@ function eventHandler(newEvent) {
                     { params : {guid: newEvent['ProcessGuid'], hostname: newEvent['Hostname']}})
                     .on("data", data => {
                         linkNewEvent(newEvent['@class'],data['@rid'], newEvent['@rid'])  
-                        if(newEvent['@class'] == 'FileCreate') console.log('Writing ' + newEvent['ProcessGuid'] + ' to cache folder for ' + newEvent['@rid'])
+                        console.log('Writing ' + newEvent['ProcessGuid'] + ' to cache folder for ' + newEvent['@rid'])
                         fs.writeFile(_cacheProcessCreateRID + '/' + newEvent['Hostname'] + newEvent['ProcessGuid'], data['@rid'], function(err) { if(err) console.log(err) });
                     })                  
                 }
