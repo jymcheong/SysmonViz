5515d63a5c906c28f6a5dedd14208b1f192c8a73
jymcheong <jym@jym.sg>
2018-11-19 15:42:41 +0800

    remove some console.log

--- serverSide/handleNonProcessCreate.js
+++ serverSide/handleNonProcessCreate.js
@@ -32,20 +32,16 @@ function eventHandler(newEvent) {
         case "FileCreateStreamHash": //ID15: ProcessCreate-[CreatedFileStream]->FileCreateStreamHash    
         case "RegistryEvent":   //ID13&14: ProcessCreate-[AccessedRegistry]->RegistryEvent
         //case "NetworkConnect":  //ID3: ProcessCreate-[ConnectedTo]->NetworkConnect 
-            //if(newEvent['@class'] == 'FileCreate') console.log('FileCreate came in...' + newEvent['@rid'])
             fs.readFile(_cacheProcessCreateRID + '/' + newEvent['Hostname'] + newEvent['ProcessGuid'], function(err, processCreateRid){
                 if(err) { // not in cache, find in database
-                    //if(newEvent['@class'] == 'FileCreate') console.log('Trying database to find for ' + newEvent['@rid']+ '...')
                     _session.query("select from (select from processcreate where ProcessGuid = :guid) where Hostname = :hostname", 
                     { params : {guid: newEvent['ProcessGuid'], hostname: newEvent['Hostname']}})
                     .on("data", data => {
                         linkNewEvent(newEvent['@class'],data['@rid'], newEvent['@rid'])  
-                        //if(newEvent['@class'] == 'FileCreate') console.log('Writing ' + newEvent['ProcessGuid'] + ' to cache folder for ' + newEvent['@rid'])
                         fs.writeFile(_cacheProcessCreateRID + '/' + newEvent['Hostname'] + newEvent['ProcessGuid'], data['@rid'], function(err) { if(err) console.log(err) });
                     })                  
                 }
-                else
-                    linkNewEvent(newEvent['@class'],processCreateRid, newEvent['@rid'])
+                else linkNewEvent(newEvent['@class'],processCreateRid, newEvent['@rid'])
             })   
             break;
 
