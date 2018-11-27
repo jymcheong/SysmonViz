21f751d22dcad3c2f584e0b0c115b7421722e374
jymcheong <jym@jym.sg>
2018-11-27 09:38:43 +0800

    refactored

--- serverSide/handleFirstSighting.js
+++ serverSide/handleFirstSighting.js
@@ -5,42 +5,61 @@ eval(fs.readFileSync(__dirname + '/common.js')+'');
 
 startLiveQuery("select from SightedTracking")
 
-function eventHandler(newEvent) {
+function linkSimilarTo(startRID, endRID) {
+    _session.command('CREATE EDGE SimilarTo FROM :h TO :c',
+    { params : {h: startRID, c: endRID}})
+    .on('error', (err)=>{
+        var msg = '' + err
+        if(msg.indexOf('modified')) {
+            linkSimilarTo(startRID, endRID)
+        }
+        else
+            console.error(msg)
+    })
+}
+
+function findCommandLineCluster(hupc){
+    _session.query("select from CommandLineCluster")
+    .all()
+    .then((results)=>{
+        var found = false
+        var i = -1
+        for(i = 0; i < results.length; i++){
+            if(jw(hupc['CommandLine'],results[i]['CommandLine']) > _threshold) {
+                found = true;
+                break;
+            }
+        }
+        if(found){
+            console.log('Found similar commandline, creating link from ' + hupc['@rid'] + ' to ' + results[i]['@rid'])
+            linkSimilarTo(hupc['@rid'], results[i]['@rid'])
+        }
+        else {
+            console.log('creating new cluster!')
+            _session.command('INSERT INTO CommandLineCluster SET CommandLine = :c', 
+            { params : {c: hupc['CommandLine']}})
+            .on('data',(cc) =>{
+                linkSimilarTo(hupc['@rid'],cc['@rid'])
+            })
+        }
+        return found
+    })
+}
+
+function eventHandler(newEvent) {   
     _session.query('SELECT FROM ' + newEvent['in'])
     .on('data', (pc)=>{
         console.log(newEvent['out'] + ':' + newEvent['@class'] + ':' + pc['@rid'])
     })
+    // Type 1 - Foreign Binaries; new Hashes
+
+    // Type 2 - Abuse Existing Tools, unusual CommandLines
     if(newEvent['@class'] == 'CommandLineSighted') {
         _session.query('SELECT FROM ' + newEvent['out'])
         .on('data', (hupc)=>{
-            _session.query("select from CommandLineCluster")
-            .all()
-            .then((results)=>{
-                var found = false
-                var i = -1
-                for(i = 0; i < results.length; i++){
-                    if(jw(hupc['CommandLine'],results[i]['CommandLine']) > _threshold) {
-                        found = true;
-                        break;
-                    }
-                }
-                if(found){
-                    console.log('Found similar commandline, creating link from ' + hupc['@rid'] + ' to ' + results[i]['@rid'])
-                    _session.command('CREATE EDGE SimilarTo FROM :h TO :c',
-                    { params : {h: hupc['@rid'], c: results[i]['@rid']}})
-                }
-                else {
-                    console.log('creating new cluster!')
-                    _session.command('INSERT INTO CommandLineCluster SET CommandLine = :c', 
-                    { params : {c: hupc['CommandLine']}})
-                    .on('data',(cc) =>{
-                        _session.command('CREATE EDGE SimilarTo FROM :h TO :c',
-                        { params : {h: hupc['@rid'], c: cc['@rid']}})
-                        .on('data', (st)=>{
-                        })
-                    })
-                }
-            })
+            findCommandLineCluster(hupc) //if existing cluster not found, higer score
         })    
     }
+
+    // Type 3 - Contents Exploitation
 }
