e74068e78e1c3aa27a1cfd52baccfcb8e844c002
jymcheong <jym@jym.sg>
2019-01-10 11:07:00 +0800

    Update handleFirstSighting.js

--- serverSide/handleFirstSighting.js
+++ serverSide/handleFirstSighting.js
@@ -5,6 +5,47 @@ eval(fs.readFileSync(__dirname + '/common.js')+'');
 
 startLiveQuery("select from SightedTracking")
 
+// startLiveQuery will call this..
+function eventHandler(newEvent) {   
+    var rid = newEvent['@class'] == 'CommandLineSighted' ? newEvent['out'] : newEvent['in'];
+    _session.query('SELECT FROM ' + rid)
+    .on('data', async (event)=>{
+        console.log(newEvent['out'] + ':' + newEvent['@class'] + ':' + event['@rid'])
+        switch(newEvent['@class']) {
+            // Type 1 - Foreign Binaries; new Hashes
+            // Deal with EXE - Foreign or NOT
+            case 'ExeSighted': // Type 1 - DLL
+                handleEXE(event); // event is a ProcessCreate
+                break;  
+
+            case 'DllSighted': // Type 1 - DLL
+                handleDLL(event); // input event is a ImageLoad, output event is ProcessCreate
+                return;   
+    
+            case 'SysSighted': // Type 1 - SYS driver
+                handleSYS(event); // event is a DriverLoad
+                return; // no need for subsequent checks for Privilege & Persistence
+    
+            case 'CommandLineSighted': // Type 2
+                handleCommandLine(event, newEvent['in']); //event is a HUPC object, 2nd param is a ProcessCreate
+                return;
+            
+            // Type 3 - Contents Exploitation that triggers new/usual process sequences that are background
+            // if foreground, it may be due to user behavior deviations
+            case  'SequenceSighted':
+                handleSequence(event); // event is a ProcessCreate
+                break;
+
+            default:
+                return;
+        }       
+        // if ProcessCreate.IntegrityLevel = High/System then +30 
+        checkPrivilege(event); //for both ExeSighted & SequenceSighted only
+        // if ProcessCreate exists BeforeExplorer then +30
+        checkBeforeExplorer(event)
+    })    
+}
+
 function linkSimilarTo(startRID, endRID) {
     _session.command('CREATE EDGE SimilarTo FROM :h TO :c',
     { params : {h: startRID, c: endRID}})
@@ -27,16 +68,24 @@ function findCommandLineCluster(hupc){
         .then((results)=>{
             var found = false
             var i = -1
+            var _clusterid = ''
+            var _clusterscore = 0
+            var _prevSimilarity = 0
             for(i = 0; i < results.length; i++){
-                if(jw(hupc['CommandLine'],results[i]['CommandLine']) > _threshold) {
+                var similarity = jw(hupc['CommandLine'],results[i]['CommandLine'])
+                if(similarity > _threshold) {
                     found = true;
-                    break;
+                    if(similarity > _prevSimilarity) {
+                        _clusterid = results[i]['@rid']
+                        _clusterscore = results[i]['Score']
+                        _prevSimilarity = similarity
+                    }
                 }
             }
             if(found){
-                console.log('Found similar commandline with score: '+ results[i]['Score'] + ', creating link from ' + hupc['@rid'] + ' to ' + results[i]['@rid'])
-                linkSimilarTo(hupc['@rid'], results[i]['@rid'])
-                resolve(results[i]['Score']) // assuming known malicious CommandLine is assigned with score
+                console.log('Found similar commandline with score: '+ _clusterscore + ', creating link from ' + hupc['@rid'] + ' to ' + _clusterid)
+                linkSimilarTo(hupc['@rid'], _clusterid)
+                resolve(_clusterscore) // assuming known malicious CommandLine is assigned with score
             }
             else {
                 _session.command('INSERT INTO CommandLineCluster SET CommandLine = :c', 
@@ -51,7 +100,7 @@ function findCommandLineCluster(hupc){
 }
 
 function linkToCase(startRID, endRID, score, reason) {
-    _session.command('CREATE EDGE AddedTo FROM :h TO :c SET score = ' + score + ", reason = '" + reason + "'",
+    _session.command('CREATE EDGE AddedTo FROM :h TO :c SET datetime = sysdate(), score = ' + score + ", reason = '" + reason + "'",
     { params : {h: startRID, c: endRID}})
     .on('error', (err)=>{
         var msg = '' + err
@@ -92,8 +141,10 @@ function handleDLL(newEvent) { // currently hardcoded to trust only Microsoft Wi
         updateCase(score,newEvent['Hostname'],newEvent['@rid'], "Foreign DLL file")
         _session.query("select expand(in('LoadedImage')) from " + newEvent['@rid'])
         .on('data', (event)=>{
-            checkPrivilege(event)
-            checkBeforeExplorer(event)
+            if(event['Image'].toLowerCase().indexOf('.exe') < 0) {
+                checkPrivilege(event)
+                checkBeforeExplorer(event)
+            }       
         })
     }
 }
@@ -111,11 +162,12 @@ async function handleCommandLine(hupc, inRid) {
     if(score > 0) {
         if(score == 30) {
             console.log('Found new CommandLine cluster!')
+            updateCase(score,hupc['Hostname'],inRid, 'Unusual CommandLine')
         }
         else {
             console.log('Using score from existing CommandLine cluster!')
+            updateCase(score,hupc['Hostname'],inRid, 'Known Malicious CommandLine')
         }
-        updateCase(score,hupc['Hostname'],inRid, 'Unusual Command Line')
         _session.query("select from " + inRid)
         .on('data', (event)=>{
             checkPrivilege(event)
@@ -132,8 +184,9 @@ function handleSequence(newEvent) {
 }
 
 function checkBeforeExplorer(processCreate){
+    console.log('checking before explorer type... ' + processCreate['ProcessType'])
     var score = processCreate['ProcessType'] == 'BeforeExplorer' ? 30 : 0;
-    if(score > 0) updateCase(score,processCreate['Hostname'],processCreate['@rid'], 'Possible Persistence Before Explorer')
+    if(score > 0) updateCase(score,processCreate['Hostname'],processCreate['@rid'], 'Executed Before Explorer')
 }
 
 function checkPrivilege(processCreate){
@@ -141,42 +194,7 @@ function checkPrivilege(processCreate){
     score = processCreate['IntegrityLevel'] == 'High' ? score + 30 : score;
     score = processCreate['IntegrityLevel'] == 'System' ? score + 30 : score;
     console.log('IntegrityLevel: ' + processCreate['IntegrityLevel'])
-    if(score > 0) updateCase(score,processCreate['Hostname'],processCreate['@rid'], 'Possible Privilege-Escalation')
+    if(score > 0) updateCase(score,processCreate['Hostname'],processCreate['@rid'], 'High-Privilege Execution')
 }
 
-function eventHandler(newEvent) {   
-    var rid = newEvent['@class'] == 'CommandLineSighted' ? newEvent['out'] : newEvent['in'];
-    _session.query('SELECT FROM ' + rid)
-    .on('data', async (event)=>{
-        console.log(newEvent['out'] + ':' + newEvent['@class'] + ':' + event['@rid'])
-        switch(newEvent['@class']) {
-            // Type 1 - Foreign Binaries; new Hashes
-            // Deal with EXE - Foreign or NOT
-            case 'ExeSighted': // Type 1 - DLL
-                handleEXE(event); // event is a ProcessCreate
-                break;  
 
-            case 'DllSighted': // Type 1 - DLL
-                handleDLL(event); // input event is a ImageLoad, output event is ProcessCreate
-                return;   
-    
-            case 'SysSighted': // Type 1 - SYS driver
-                handleSYS(event); // event is a DriverLoad
-                return; // no need for subsequent checks for Privilege & Persistence
-    
-            case 'CommandLineSighted': // Type 2
-                handleCommandLine(event, newEvent['in']); //event is a HUPC object, 2nd param is a ProcessCreate
-                return;
-            
-            // Type 3 - Contents Exploitation that triggers new/usual process sequences that are background
-            // if foreground, it may be due to user behavior deviations
-            case  'SequenceSighted':
-                handleSequence(event); // event is a ProcessCreate
-                break;
-        }       
-        // if ProcessCreate.IntegrityLevel = High/System then +30 
-        checkPrivilege(event); //for both ExeSighted & SequenceSighted only
-        // if ProcessCreate exists BeforeExplorer then +30
-        checkBeforeExplorer(event)
-    })    
-}
