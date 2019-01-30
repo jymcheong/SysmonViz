11be6688933fff10879670b65d2a13eaf4eef3f1
jymcheong <jym@jym.sg>
2019-01-30 13:30:00 +0800

    Update handleFirstSighting.js

--- serverSide/handleFirstSighting.js
+++ serverSide/handleFirstSighting.js
@@ -209,6 +209,9 @@ function handleLateralComm(newEvent) {
         if(newEvent['Image'] == 'C:\\Windows\\System32\\svchost.exe' && newEvent['SourcePortName'] == 'ssdp') {
             return
         }
+        if(newEvent['Image'] == 'C:\\Windows\\System32\\svchost.exe' && newEvent['SourcePortName'] == 'llmr') {
+            return
+        }
         if(newEvent['Image'] == 'C:\\Windows\\System32\\dasHost.exe' && newEvent['SourcePortName'] == 'ws-discovery') {
             return
         }
