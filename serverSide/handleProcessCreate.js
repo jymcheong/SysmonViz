22c4da62ba86457f547830a4c8c57d757f7f0354
jymcheong <jym@jym.sg>
2019-03-07 16:41:10 +0800

    refactored parentof fixing

--- serverSide/handleProcessCreate.js
+++ serverSide/handleProcessCreate.js
@@ -20,62 +20,66 @@ function eventHandler(newpc) { // a fix function name that is used within startL
 
 setInterval(function(){ processQueue()},100); 
 
-function processQueue(){
-    if(_processCreateQ.length == 0){ return; }
-    var newpc = _processCreateQ[0];
-    var parentRID = _mapProcessCreate.get(newpc['Hostname'] + newpc['ParentProcessGuid'])
-    if(parentRID) {
-        if(newpc['ParentImage'] == 'C:\\Windows\\System32\\svchost.exe' && newpc['Image'] == 'C:\\Windows\\System32\\wininit.exe') {
-            console.log('Circular path found... ' + newpc['@rid']) // the following fixes a Sysmon BUG
-            _session.query("select from pc Where ParentImage like '%smss.exe' AND Image like '%smss.exe' AND ProcessId = :id order by id desc limit 1", 
-            { params : {id: newpc['ParentProcessId']}}) // find the real parent
-            .all()
-            .then((data)=> {
-                if(data.length > 0){ // correct the child process parent-related-fields
-                    _session.command("UPDATE " + newpc['@rid'] + " SET ParentProcessGuid = :p1, ParentProcessId = :p2, ParentImage = :p3, ParentCommandLine = :p4"
-                    ,{ params : {p1: data[0]['ProcessGuid'], p2: data[0]['ProcessId'], p3: data[0]['Image'], p4: data[0]['CommandLine'] }} )
-                    .on('data',(results)=> {
-                        console.log('Updated parent process fields for affected entry...' + newpc['@rid'])
-                        parentRID = data[0]['@rid']
-                        connectParentOf(parentRID, newpc['@rid']) // link to the correct parent
-                        _processCreateQ.shift()
-                    })
-                }
-                else {
-                    console.log('Cannot find smss.exe, give up...')
-                    _processCreateQ.shift()
-                }
+
+function fixParent(newpc) {
+    _session.query("select from pc Where ParentImage like '%smss.exe' AND Image like '%smss.exe' AND ProcessId = :id order by id desc limit 1", 
+    { params : {id: newpc['ParentProcessId']}}) // find the real parent
+    .all()
+    .then((data)=> {
+        if(data.length > 0){ // correct the child process parent-related-fields
+            _session.command("UPDATE " + newpc['@rid'] + " SET ParentProcessGuid = :p1, ParentProcessId = :p2, ParentImage = :p3, ParentCommandLine = :p4"
+            ,{ params : {p1: data[0]['ProcessGuid'], p2: data[0]['ProcessId'], p3: data[0]['Image'], p4: data[0]['CommandLine'] }} )
+            .on('data',(results)=> {
+                console.log('Updated parent process fields for affected entry...' + newpc['@rid'])
+                parentRID = data[0]['@rid']
+                connectParentOf(parentRID, newpc['@rid']) // link to the correct parent
+                _processCreateQ.shift()
             })
         }
         else {
+            console.log('Cannot find smss.exe, retrying later...')
+        }
+    })
+}
+
+function processQueue(){
+    if(_processCreateQ.length == 0) return; 
+    var newpc = _processCreateQ[0];
+    if(newpc['ParentImage'].indexOf('svchost.exe') > 0 && (newpc['Image'].indexOf('wininit.exe') > 0 || newpc['Image'].indexOf('csrss.exe') > 0)) {
+        console.log('Circular path found... ' + newpc['@rid']) // the following fixes a Sysmon BUG
+        fixParent(newpc)
+    }
+    else {
+        var parentRID = _mapProcessCreate.get(newpc['Hostname'] + newpc['ParentProcessGuid'])
+        if(parentRID) {
             connectParentOf(parentRID, newpc['@rid'])
             _processCreateQ.shift()
         }
-    }
-    else {
-        console.log('Searching database for parent of ' + newpc['Image'])
-        _session.query("select @rid from (select from processcreate where ProcessGuid = :guid) where Hostname = :hostname", 
-        { params : {guid: newpc['ParentProcessGuid'], hostname: newpc['Hostname']}})
-        .all()
-        .then((data)=> {
-            if(data.length > 0) {
-                console.log('Found parent RID from DB for ' + newpc['Image'])
-                _mapProcessCreate.set(newpc['Hostname'] + newpc['ParentProcessGuid'], data[0]['@rid'])
-                fs.writeFile(_cacheProcessCreateRID + '/' + newpc['Hostname'] + newpc['ParentProcessGuid'], data[0]['@rid'], function(err) { if(err) console.log(err) });
-            }
-            else { // retry...
-                var key = newpc['ParentProcessGuid'] + newpc['Hostname']
-                if(key in _retries) {
-                    if(_retries[key] == 10) { // max wait for 1 sec (10 x 100ms interval)
-                        console.log('Cannot find ' + newpc['ParentProcessGuid'] + ' for '+ newpc['Image'] + ' on ' + newpc['Hostname'])
-                        _processCreateQ.shift()
-                        fs.writeFile(_cacheProcessCreateRID + '/MISSING-' + newpc['Hostname'] + newpc['ParentProcessGuid'], '', function(err) { if(err) console.log(err) });
-                    }
-                    else { _retries[key]++ } // max wait is only done once
+        else {
+            console.log('Searching database for parent of ' + newpc['Image'])
+            _session.query("select @rid from (select from processcreate where ProcessGuid = :guid) where Hostname = :hostname", 
+            { params : {guid: newpc['ParentProcessGuid'], hostname: newpc['Hostname']}})
+            .all()
+            .then((data)=> {
+                if(data.length > 0) {
+                    console.log('Found parent RID from DB for ' + newpc['Image'])
+                    _mapProcessCreate.set(newpc['Hostname'] + newpc['ParentProcessGuid'], data[0]['@rid'])
+                    fs.writeFile(_cacheProcessCreateRID + '/' + newpc['Hostname'] + newpc['ParentProcessGuid'], data[0]['@rid'], function(err) { if(err) console.log(err) });
                 }
-                else { _retries[key] =  1 }
-            }
-        });
+                else { // retry...
+                    var key = newpc['ParentProcessGuid'] + newpc['Hostname']
+                    if(key in _retries) {
+                        if(_retries[key] == 10) { // max wait for 1 sec (10 x 100ms interval)
+                            console.log('Cannot find ' + newpc['ParentProcessGuid'] + ' for '+ newpc['Image'] + ' on ' + newpc['Hostname'])
+                            _processCreateQ.shift()
+                            fs.writeFile(_cacheProcessCreateRID + '/MISSING-' + newpc['Hostname'] + newpc['ParentProcessGuid'], '', function(err) { if(err) console.log(err) });
+                        }
+                        else { _retries[key]++ } // max wait is only done once
+                    }
+                    else { _retries[key] =  1 }
+                }
+            });
+        }
     }
 }
 
