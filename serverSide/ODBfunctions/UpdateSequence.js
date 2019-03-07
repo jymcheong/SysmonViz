1664bf5e13c43949bc60a5cb7b4b72930c06f6db
jymcheong <jym@jym.sg>
2019-03-07 13:46:31 +0800

    Update UpdateSequence.js

--- serverSide/ODBfunctions/UpdateSequence.js
+++ serverSide/ODBfunctions/UpdateSequence.js
@@ -19,37 +19,32 @@ function linkSequenceToProcessCreate(sequenceRid) {
   }
 }
 
-function updateSequence(){
-	var exename = doc.field('in').field('Image').split("\\")
-    exename = exename[exename.length - 1]
-    for(var i = 0; i < 6; i++){ //retry mechanism
-      try{
-         var prevSeq = '' + doc.field('out').field('Sequence');
-         if(prevSeq.indexOf('System') < 0) {
-            print('Found partial sequence, attempt to fix: ' + prevSeq)
-         	var ps = db.query('SELECT GetParentOfSequence(?) as seq', doc.field('out').field('@rid'))
-            prevSeq = ps[0].field('seq')
-            if(prevSeq.indexOf('System') < 0) continue;
-            print('Sequence from GetParentOfSequence: ' + prevSeq);
-            db.command('UPDATE ? SET Sequence = ? RETURN AFTER Sequence', doc.field('out').field('@rid'), prevSeq)
-         }
-         var s = db.command('UPDATE ? SET Sequence = ? RETURN AFTER Sequence', doc.field('in').field('@rid'),
-                   prevSeq + ' > ' + exename) //updates ProcessCreate vertice
-         
-         var sc = db.command('UPDATE ParentOfSequence SET Count = Count + 1 \
-				  UPSERT RETURN AFTER @rid, Count, Score WHERE Sequence = ?',s[0].field('Sequence')) 
-         
-         print(sc[0].field('Count') + '|'+ s[0].field('Sequence'));
-         if(sc[0].field('Score') > 0 || sc[0].field('Count') == 1)
-		 	linkSequenceToProcessCreate(sc[0].field('@rid'))
-        
-         break;
+var exename = doc.field('in').field('Image').split("\\")
+exename = exename[exename.length - 1]
+for(var i = 0; i < 6; i++){ //retry mechanism
+  try{
+      var prevSeq = '' + doc.field('out').field('Sequence');
+      if(prevSeq.indexOf('System') < 0) {
+        print('Found partial sequence, attempt to fix: ' + prevSeq)
+      var ps = db.query('SELECT GetParentOfSequence(?) as seq', doc.field('out').field('@rid'))
+        prevSeq = ps[0].field('seq')
+        if(prevSeq.indexOf('System') < 0) continue;
+        print('Sequence from GetParentOfSequence: ' + prevSeq);
+        db.command('UPDATE ? SET Sequence = ? RETURN AFTER Sequence', doc.field('out').field('@rid'), prevSeq)
       }
-      catch(err){
-         var e = '' + err
-		 if(e.indexOf('UPDATE') >= 0) continue; 
-      }
-    }
+      var s = db.command('UPDATE ? SET Sequence = ? RETURN AFTER Sequence', doc.field('in').field('@rid'),
+                prevSeq + ' > ' + exename) //updates ProcessCreate vertice
+      
+      var sc = db.command('UPDATE ParentOfSequence SET Count = Count + 1 \
+      UPSERT RETURN AFTER @rid, Count, Score WHERE Sequence = ?',s[0].field('Sequence')) 
+      
+      print(s[0].field('Sequence') + '|' + sc[0].field('Count'));
+      if(sc[0].field('Score') > 0 || sc[0].field('Count') == 1) linkSequenceToProcessCreate(sc[0].field('@rid'))
+    
+      break;
+  }
+  catch(err){
+      var e = '' + err
+  if(e.indexOf('UPDATE') >= 0) continue; 
+  }
 }
-
-updateSequence()
