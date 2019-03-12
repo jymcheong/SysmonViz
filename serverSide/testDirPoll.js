5ff07bfc04560f1b19e7392c4c555e8f450c98c7
Jym Cheong <jym@jym.sg>
2019-03-12 09:25:23 +0800

    Delete testDirPoll.js

--- serverSide/testDirPoll.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs")
-
-function pollDirectory(directory_to_monitor){
-    fs.readdir(directory_to_monitor, function(err, items) {
-        console.log(items)
-    })
-}
-
-setInterval(function(){ pollDirectory('/Users/jymcheong/eventUpload')},1000)
